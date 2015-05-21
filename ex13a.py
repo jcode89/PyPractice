95e0749a018e4a5763274c6009516638353fce76
Jonathan Colon <jcolon.basementcoders@gmail.com>
2015-05-21 14:17:08 -0400

    Initial Commit

---
+++ ex13a.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, xi, yi, zi, al, ba, ci = argv
+
+print '''Your script is %r 
+\nThen you typed all of this other junk:
+\n%r,%r,%r,%r,%r,%r
+''' % (script, xi, yi, zi, al, ba, ci)
+
