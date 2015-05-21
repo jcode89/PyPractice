5ac36941eaa2748f9524a19ecde24f55e4797059
Jonathan Colon <jcolon.basementcoders@gmail.com>
2015-05-21 14:15:18 -0400

    Initial Commit

---
+++ ex7.py
@@ -0,0 +1,42 @@
+#The print function is called to print out the given string
+print "Mary had a little lamb."
+#The print function is called to print out the given string 
+#while adding the string snow to the original string using the string formatter %s
+print "Its fleece was white as %s" % 'snow'
+#Print function called to print out the given string
+print "And everywhere that Mary went."
+#Print function called to print out . but because *10 is at the end it will
+#multiply the string ten times
+print "." * 10 #what'd that do?
+
+#Assigns the string C to end1
+end1 = "C"
+#Assigns the string h to end2
+end2 = "h"
+#Assigns the string e to end3
+end3 = "e"
+#Assigns the string e to end4
+end4 = "e"
+#Assigns the string s to end5
+end5 = "s"
+#Assigns the string e to end6
+end6 = "e"
+#Assigns the string B to end7
+end7 = "B"
+#Assigns the string u to end8
+end8 = "u"
+#Assigns the string r to end9
+end9 = "r"
+#Assigns the string g to end10
+end10 = "g"
+#Assigns the string e to end11
+end11 = "e"
+#Assigns the string r to end12
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+#Print function is called to print out end1-end6 and concatenate them
+#The comma at the end tells python to place the second print functions results on the same line as the first
+print end1 + end2 + end3 + end4 + end5 + end6,
+#Print function is called to print out end7-end12 and concatenate them as well.
+print end7 + end8 + end9 + end10 + end11 + end12
