98504f04b37dafd6370506963b7b920bfb09fbe4
JCode89 <JV.Colon@Yahoo.com>
2015-07-07 17:32:28 -0400

    Prints multiple values.

--- ex39_test.py
+++ ex39_test.py
@@ -9,22 +9,30 @@ hashmap.set(states, 'Florida', 'FL')
 hashmap.set(states, 'California', 'CA')
 hashmap.set(states, 'New York', 'NY')
 hashmap.set(states, 'Michigan', 'MI')
-
+hashmap.set(states, 'Oregon', 'US')
+hashmap.set(states, 'Florida', 'US')
+hashmap.set(states, 'California', 'US')
+hashmap.set(states, 'New York', 'US')
+hashmap.set(states, 'Michigan', 'US')
 # create a basic set of states and some cities in them
 cities = hashmap.new()
 hashmap.set(cities, 'CA', 'San Francisco')
+hashmap.set(cities, 'CA', 'San Diego')
 hashmap.set(cities, 'MI', 'Detroit')
+hashmap.set(cities, 'MI', 'Freezing')
 hashmap.set(cities, 'FL', 'Jacksonville')
-
+hashmap.set(cities, 'FL', 'Tampa Bay')
+hashmap.set(cities, 'FL', 'Miami')
 # add some more cities
 hashmap.set(cities, 'NY', 'New York')
 hashmap.set(cities, 'OR', 'Portland')
-
-
+hashmap.set(cities, 'OR', 'Helena')
+hashmap.set(cities, 'NY', 'Brooklyn')
+#hashmap.delete(cities, 'NY')
 # print out some cities
 print '-' * 10
-print "NY State has: %s" % hashmap.get(cities, 'NY')
-
+print "NY State has: %s " % hashmap.get(cities, 'NY')
+print "FL State has: %s " % hashmap.get(cities, 'FL')
 print "OR State has: %s" % hashmap.get(cities, 'OR')
 
 # print some states
@@ -35,9 +43,9 @@ print "Florida's abbreviation is: %s" % hashmap.get(states, 'Florida')
 
 # do it by using the state then cities dict
 print '-' * 10
-print "Michigan has: %s" % hashmap.get(cities, hashmap.get(states, 'Michigan'))
+#print "Michigan has: %s" % hashmap.get(cities, hashmap.get(states, 'Michigan'))
 
-print "Florida has: %s" % hashmap.get(cities, hashmap.get(states, 'Florida'))
+#print "Florida has: %s" % hashmap.get(cities, hashmap.get(states, 'Florida'))
 
 # print every state abbreviation
 print '-' * 10
@@ -45,7 +53,7 @@ hashmap.list(states)
 
 # print every city in state
 print '-' * 10
-hashmap.delete(cities, 'OR')
+#hashmap.delete(cities, 'OR')
 hashmap.list(cities)
 
 print '-' * 10
@@ -59,12 +67,13 @@ if not state:
 city = hashmap.get(cities, 'TX', 'Does Not Exist')
 print "The city for the state 'TX' is: %s" % city
 
-hashmap.dump(cities)
+#hashmap.dump(cities)
 print '-' * 10
 #hashmap.dump(states)
-assert(hashmap.get(cities, 'NY') == 'New York')
-assert(hashmap.get(cities, 'OR') == 'Portland')
-assert(hashmap.get(states, 'Michigan') == 'MI')
-assert(hashmap.get(states, 'Florida') == 'FL')
-assert(hashmap.get(cities, hashmap.get(states, 'Michigan'))in 'Detroit')
-assert(hashmap.get(cities, hashmap.get(states, 'Florida')) in 'Jacksonville')
+# The following assertions test if there is more than one value.
+assert(hashmap.get(cities, 'NY') != 'New York')
+assert(hashmap.get(cities, 'OR') != 'Portland')
+assert(hashmap.get(states, 'Michigan') != 'MI')
+assert(hashmap.get(states, 'Florida') != 'FL')
+#assert(hashmap.get(cities, hashmap.get(states, 'Michigan'))in 'Detroit')
+#assert(hashmap.get(cities, hashmap.get(states, 'Florida')) in 'Jacksonville')
