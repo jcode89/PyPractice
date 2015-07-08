29dfa4c7ea90ab3ca904a2f452b4fa8f5046ef33
JCode89 <JV.Colon@Yahoo.com>
2015-07-08 01:24:07 -0400

    Uses different methods to interact with dicts.

--- ex39.py
+++ ex39.py
@@ -54,3 +54,43 @@ if not state:
 # get a city with a default values
 city = cities.get('TX', 'Does Not Exist')
 print "The city for the state of 'TX' is: %s" % city
+print "But we will add it!"
+print "And we are going to add a surprise!"
+states.update({'Texas':'TX'})
+cities.update({'TX':'San Antonio'})
+states.setdefault('Louisiana','LA')
+cities.setdefault('LA',[])
+cities['LA'].append('New Orleans')
+cities['LA'].append('Shreveport')
+print '-' * 10
+for state, abbrev in states.items():
+    print "%s state is abbreviated %s and has city %s" % (
+        state, abbrev, cities[abbrev])
+print '-' * 10
+print "Maybe multiple values? How about a list of values?"
+cities['TX'] = ['San Antonio', 'Houston', 'Dallas']
+for state, abbrev in states.items():
+    print "%s state is abbreviated %s and has city %s" % (
+        state, abbrev, cities[abbrev])
+		
+print '-' * 10
+cars = {
+    'Chevy': 'Malibu',
+	'Ford': 'F150',
+	'Honda': 'Civic',
+	'Nissan': 'Sentra',
+	'Toyota': 'Camry'
+}
+
+more_cars = {
+    'Chevy': 'Camaro',
+	'Chevy': 'Corvette',
+	'Mazda': 'Spyder',
+	'Rolls Royce': 'Grand'
+	
+}
+
+cars.update(more_cars)
+
+for key, value in cars.items():
+    print "%s has value %s" % (key, value)
