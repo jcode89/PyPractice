a5965ccd30439c16fa66924685039067ed870fad
JCode89 <JV.Colon@Yahoo.com>
2015-07-14 20:53:30 -0400

    Grabs the titles of each issue.

---
+++ issues.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+# requests the information from the site.
+r = requests.get('https://api.github.com/repos/code-newbies/python-thursday-adventure/issues')
+
+# This will grab the text of the response either as unicode(text)
+# or bytes(content)
+# It then deserializes the content into a useable python object using
+# json.loads
+data = json.loads(r.text or r.content) 
+
+print (len(data))
+
+# each issue is an item in a list.
+# using the for loop we can grab its index and call that item,
+# it then acts like any normal JSON, which acts like a dict
+# so we can call values using the keys.
+for index in range(0, len(data)):
+    print (data[index]['title'])
