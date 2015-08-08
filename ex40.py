f5640a6f9ce8e8e56edd09f84c2cdf24dcdb72b8
jcode89 <jv.colon@yahoo.com>
2015-08-08 16:29:43 -0400

    Adds print statements to clarify what is happening, so the lesson is learned.

--- ex40.py
+++ ex40.py
@@ -24,30 +24,39 @@ bulls_on_parade = Song(
 taylor_swift_bad_blood = ["And it's so sad too",
         "Think about the good times",
         "You and I."]
-        
+# For clarity statements will be printed that explain each item.
+print "These are the original untouched Lyrics: "      
 # Calls the sing_me_a_song() method and prints out the lyrics one line at a time.     
 happy_bday.sing_me_a_song()
-
+print "\n"
+print "These are the original untouched Lyrics: "
 # Does the same as the call above.
 bulls_on_parade.sing_me_a_song()
-
+print "\n"
+print "The last line of the last song, that will be deleted: "
 # This calls the last_line() method which removes the last lyric from the list and displays it
 bulls_on_parade.last_line()
-
+print "\n"
+print "Add a line to the Happy birthday song. "
 # This prompts you to add a lyric to the song.
 happy_bday.add_me_a_line()
-
+print "\n"
+print "The happy birthday song with the new lyric: "
 # This will print out the lyrics including the new one you just added.
 happy_bday.sing_me_a_song()
-
+print "\n"
+print "Add a lyric to bulls on parade. "
 # Prompts to add a lyric
 bulls_on_parade.add_me_a_line()
-
+print "\n"
+print "The new lyrics, and the last line still removed: "
 # Displays the new lyrics, the last line still removed and the new lyric added.
 bulls_on_parade.sing_me_a_song()
-
+print "\n"
+print "New lyrics just instantiated."
 # For this we instantiate the class and pass it the variable taylor_swift_bad_blood
 tay_swift = Song(taylor_swift_bad_blood)
-
+print "\n"
+print "The new lyrics: "
 # This calls the sing_me_a_song method and displays the lyrics.
 tay_swift.sing_me_a_song()
