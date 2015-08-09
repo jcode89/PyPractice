4a44a9f1b1637f8d5b88aa1bc1dbbd822e929cca
jcode89 <jv.colon@yahoo.com>
2015-08-09 00:00:23 -0400

    Adds more arguments to class Person, and another method.

--- ex42.py
+++ ex42.py
@@ -37,27 +37,34 @@ class Cat(Animal):
 ## Person is-a object
 class Person(object):
 
-    def __init__(self, name):
+    def __init__(self, name, age=0, height="", outgoing=None):
         ## Person has-a name
         self.name = name
         ## Person has-a pet of some kind
         self.pet = None
+        self.age = age
+        self.height = height
+        self.outgoing = outgoing
 
     def print_name(self):
         print self.name
+
+    def features(self):
+        ToF = {"age": self.age, "height": self.height, "personality": self.outgoing}
+        return ToF
 
 
 ## Employee is-a Person
 class Employee(Person):
 
-    def __init__(self, name, salary):
+    def __init__(self, name, salary, age, height, outgoing):
         ## ?? hmm what is this strange magic?
         ## Employee has-a name
-        super(Employee, self).__init__(name)
+        super(Employee, self).__init__(name, age, height, outgoing)
         ## Employee has-a salary
         self.salary = salary
         super(Employee, self).print_name()
-
+        super(Employee, self).features()
 
 
 ## Fish is-a object
@@ -85,8 +92,8 @@ class Halibut(Fish):
         super(Halibut, self).__init__("brown", "yellow", "medium")
         super(Halibut, self).features()
 
-'''This exercise is almost finished. The next step is to make new
-relationships that are either lists or dictionaries.'''
+'''I have added in a dictionary for the Person class, but **kwargs
+probably could have been used.'''
 
 ## Rover is-a Dog
 rover = Dog("Rover")
@@ -95,17 +102,24 @@ rover.hairy()
 satan = Cat("Satan")
 satan.has_hair()
 ## Mary is-a Person
-mary = Person("Mary")
+mary = Person("Mary", 26, "5Ft 4in", True)
 mary.print_name()
+print "Mary's age is %r, her height is %r." % (mary.features()['age'], mary.features()['height'])
+if mary.features()['personality']:
+    print "Mary is very outgoing!"
 ## Mary has-a pet and her pet is-a cat and her cat has-a name
 mary.pet = satan
 mary.pet.has_hair()
 ## frank is-a Employee
-frank = Employee("Frank", 120000)
+frank = Employee("Frank", 120000, 36, "6ft", False)
 frank.print_name
+print "Frank makes %d" % frank.salary
+print "Frank is %d years old and is %s tall." % (frank.features()['age'], frank.features()['height'])
+if not frank.features()['personality']:
+    print "Frank is not very outgoing!"
 ## Frank has-a pet, his pet is-a dog, his dog has-a name
 frank.pet = rover
-
+frank.pet.hairy()
 ## flipper is-a Fish
 flipper = Fish("grey", "black", "super long")
 flipper.features()
@@ -114,3 +128,13 @@ crouse = Salmon()
 
 ## harry is-a Halibut
 harry = Halibut()
+# barry is a dictionary and each key in the dictionary is a class
+barry ={"Fish": Fish("Yellow", "Green", "Puffy"), "Animal": Animal("Puffer Fish", False)}
+## I did not realize I used features as a method in two seperate classes.
+barry["Fish"].features()
+barry["Animal"].features()
+
+# barry could also be a list
+barry = [Fish("Yellow", "Green", "Puffy"), Animal("Puffer Fish", False)]
+barry[1].features()
+barry[0].features()
